1a258070b940619b806719ad04dc769f5faef09c
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-08 09:09:55 +0700

    LKS Kabupaten Purbalingga 2024 | Web Technologies - Client Slide - Page Content, Page Instruction

---
+++ client slide/script.js
@@ -0,0 +1,21 @@
+let instruction = document.getElementById("screenInstruction");
+let btnStart = document.getElementById("startGame");
+let username = document.getElementById("username");
+
+function openInstruction() {
+  instruction.style.display = "flex";
+}
+
+function closeInstruction() {
+  instruction.style.display = "none";
+}
+
+username.addEventListener("input", () => {
+  if (username.value.length > 0) {
+    btnStart.disabled = false;
+  }
+});
+
+btnStart.addEventListener("click", () => {
+  window.location.href = "test.html";
+});
